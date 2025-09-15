5ed8402e6dd80e1ec233c8bdc8cbf2face178308
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2025-09-15 18:38:33 +0700

    Fresh

--- app/pages/about.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Tentang Kami</h1>
-    <NuxtLink to="/">Kembali ke Home</NuxtLink>
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,5 @@
-import Aura from '@primeuix/themes/aura';
-import tailwindcss from "@tailwindcss/vite";
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "Nuxt App",
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    },
-
-  },
-  devtools: { enabled: false },
-  css: [
-    "@/assets/main.css"
-  ],
-  modules: ["@primevue/nuxt-module"],
-  primevue: {
-    options: {
-      theme: {
-        preset: Aura,
-        options: {
-          darkModeSelector: ".p-dark",
-        }
-      },
-      ripple: true
-    },
-    autoImport: true
-  },
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ]
-  },
-  compatibilityDate: "2025-02-26"
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
